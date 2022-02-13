ba9d650d6a7fac211c0175b5e33094a576eeecec
OrangeHan0x01 <69062812+OrangeHan0x01@users.noreply.github.com>
2022-02-13 16:47:51 +0800

    Update pdrec.py

--- audio_process/pdrec.py
+++ audio_process/pdrec.py
@@ -2,15 +2,20 @@ import paddle
 from paddlespeech.cli import ASRExecutor
 from paddlespeech.cli import STExecutor
 
-def pd_asr(file):#语音识别模块
+def pd_asr(file,tgt_lang='zh'):#中文语音识别
 	asr_executor = ASRExecutor()
-	text = asr_executor(model='conformer_wenetspeech',lang='zh',sample_rate=16000,config=None,ckpt_path=None,audio_file=file,force_yes=False,device=paddle.get_device())
+	if(tgt_lang=='zh'):
+		text = asr_executor(model='conformer_wenetspeech',lang=tgt_lang,sample_rate=16000,config=None,ckpt_path=None,audio_file=file,force_yes=False,device=paddle.get_device())
+	elif(tgt_lang=='en'):
+		text = asr_executor(model='transformer_librispeech',lang=tgt_lang,sample_rate=16000,config=None,ckpt_path=None,audio_file=file,force_yes=False,device=paddle.get_device())
+	else:
+		text='目前只能识别中文和英文'
 	print('识别结果：',text)
 	return text
 
 
-def pd_st(file):#语音英译中模块
+def pd_st(file,srl='en',tgl='zh'):#英文语音翻译
 	st_executor = STExecutor()
-	text = st_executor(model='fat_st_ted',src_lang='en',tgt_lang='zh',sample_rate=16000,config=None,ckpt_path=None,audio_file=file,device=paddle.get_device())
+	text = st_executor(model='fat_st_ted',src_lang=srl,tgt_lang=tgl,sample_rate=16000,config=None,ckpt_path=None,audio_file=file,device=paddle.get_device())
 	print('翻译结果：',text)
 	return text
